632b48dcc73873711162ffb5cc4717646883bcc5
Aditya Pathak <sumitpathak861@gmail.com>
2024-03-31 02:13:29 +0530

    switch url.

--- nodeJs/http/index.js
+++ nodeJs/http/index.js
@@ -1,6 +1,21 @@
 const http = require("http");
+const fs = require("fs");
+const { timeStamp } = require("console");
 
-const myServer = http.createServer();
+const myServer = http.createServer((req, res) => {
+  const log = `New Request Received on:${Date.now()}:, Path:${req.url}`;
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (req.url) {
+      case "/":
+        res.end("Homepage");
+        break;
+      case "/about":
+        res.end("About Page");
+        break;
+      default:
+        res.end("404 Not Found");
+    }
+  });
+});
 
-
-
+myServer.listen(3000, () => console.log("Server Created Successfully"));
